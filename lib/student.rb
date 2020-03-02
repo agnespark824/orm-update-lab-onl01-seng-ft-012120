70b97d1d0386cb591d835d7a7981fd19ba33fa84
Agnes Park <agnespark824@gmail.com>
2020-03-02 01:56:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,8 @@ class Student
     sql = <<-SQL
       INSERT INTO students(name, grade)
       VALUES (?, ?);
-    SQL
+      SQL
+      
     DB[:conn].execute(sql, self.name, self.grade)
   end
   
