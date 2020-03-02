78fba189d25dd13dad02490e99450f1c7b03a2c9
Agnes Park <agnespark824@gmail.com>
2020-03-02 02:15:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,8 @@ class Student
         WHERE name = ?
       SQL
       
-    DB[:conn].execute(sql, name).map do |
+    DB[:conn].execute(sql, name).map do | row |
+      new_from_db(row)
   end
   
   def update
