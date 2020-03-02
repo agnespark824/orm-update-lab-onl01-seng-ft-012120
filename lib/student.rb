89c50d242159a8a7b6ee5295071f4f7b85e08186
Agnes Park <agnespark824@gmail.com>
2020-03-02 02:18:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,4 +71,10 @@ class Student
   def update
     
   end
+  
+    def update
+    sql = "UPDATE songs SET name = ?, album = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.name)
+  end
+end
 end
