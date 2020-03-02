e56d5a125a22fdee3592d3e44f61de59fae753ec
Agnes Park <agnespark824@gmail.com>
2020-03-02 02:07:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,9 +42,15 @@ class Student
   end
   
   def self.create(name, grade)
-    student = Student.new(name, grade)
+    student = Student.new(name, grade, id=nil)
     student.save
-    student
   end
+  
+  def self.new_from_db(array)
+    id = array[0]
+    name = array[1]
+    grade = array[2]
+    
+    
   
 end
